6331715cb29eaaaac135208455a371054bf929ae
ABHI <abhichoudhary3219@gmail.com>
2025-04-28 15:38:25 +0530

    login completed

---
+++ script1.js
@@ -0,0 +1,23 @@
+let registerBtn = document.getElementById("click1")
+let showForm = document.getElementById("registrationForm")
+let container = document.getElementById("container")
+
+registerBtn.addEventListener("click", function(){
+    showForm.style.display = "block"
+    // registerBtn.style.color = "red"
+    container.style.display = "none"
+
+
+})
+let backbtn = document.getElementById("back")
+backbtn.addEventListener ("click", function () {
+    showForm.style.display = "none"
+    container.style.display = "block"
+
+})
+let backHomebtn = document.getElementById("backHome")
+backHomebtn.addEventListener ("click", function () {
+    showForm.style.display = "none"
+    container.style.display = "block"
+
+})
