0ea25e9263bdc67941e8b8615e5ea32d78423c87
Anshul <choudharyanshul220@gmail.com>
2025-05-01 07:59:51 +0530

    fixing the u of registration form and add the password field in it

--- script1.js
+++ script1.js
@@ -1,12 +1,13 @@
 let registerBtn = document.getElementById("click1")
 let showForm = document.getElementById("registrationForm")
 let container = document.getElementById("container")
+let homePage=document.querySelector("#homePage")    
 
 registerBtn.addEventListener("click", function(){
     showForm.style.display = "block"
     // registerBtn.style.color = "red"
     container.style.display = "none"
-
+homePage.style.filter="blur(10px)"
 
 })
 let backbtn = document.getElementById("back")
@@ -19,5 +20,6 @@ let backHomebtn = document.getElementById("backHome")
 backHomebtn.addEventListener ("click", function () {
     showForm.style.display = "none"
     container.style.display = "block"
+    homePage.style.filter="blur(0px)"
 
 })
